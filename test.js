0bd36034da95a6faac67c2161e13364e24110289
Drashti312812 <ardeshnadrashti@gmail.com>
2024-12-23 18:15:28 +0530

    Created new branch and uncommented code

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
-// x = findMax(1, 123, 500, 115, 44, 88);
+x = findMax(1, 123, 500, 115, 44, 88);
 
-// function findMax() {
-//   let max = -Infinity;
-//   for (let i = 0; i < arguments.length; i++) {
-//     if (arguments[i] > max) {
-//         console.log('arguments: ', arguments[i]);
-//       max = arguments[i];
-//     }
-//   }
-//   return max;
-// }
+function findMax() {
+  let max = -Infinity;
+  for (let i = 0; i < arguments.length; i++) {
+    if (arguments[i] > max) {
+        console.log('arguments: ', arguments[i]);
+      max = arguments[i];
+    }
+  }
+  return max;
+}
 
